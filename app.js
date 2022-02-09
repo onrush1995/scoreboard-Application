135158cf6febd7bece705e29412965792cc54806
Moni <moniruzzaman.eng1995@gmail.com>
2022-02-09 11:53:48 +0200

    Added JSX to replace React CreateElement

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const title = React.createElement('h1', { id: 'main-title', title: 'This is a title' }, 'My Frist React Element');
+const desc = 'I just learned how to create a React node and render it into the DOM';
+const myTitleID = 'main-title';
+const name_ID = 'Moni';
 
-const desc = React.createElement('p', null, 'I just learned how to create a React node and render it into the DOM');
-
-const header = React.createElement('header', null, title, desc);
+const header = (
+    <header>
+        <h1>{name_ID}'s first React Element</h1>
+        <p>{desc}</p>
+    </header>
+);
 
 ReactDOM.render(header, document.getElementById('root'));
