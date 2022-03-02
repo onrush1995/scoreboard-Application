deccd6d807c4358a0cf6a7cef066753b7a17255e
Moni <moniruzzaman.eng1995@gmail.com>
2022-03-02 15:10:24 +0200

    Twiking app.js file

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const desc = 'I just learned how to create a React node and render it into the DOM';
 const myTitleID = 'main-title';
-const name_ID = 'Moni';
+const firstName = 'Moni';
 
 const header = (
     <header>
-        <h1>{name_ID}'s first React Element</h1>
+        {/* This is comment */}
+        <h1>{firstName}'s first React Element</h1>
         <p>{desc}</p>
     </header>
 );
