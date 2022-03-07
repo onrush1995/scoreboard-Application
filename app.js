778c93739262bd0638eab40f2f81de3966ac4430
Moni <moniruzzaman.eng1995@gmail.com>
2022-03-07 14:37:29 +0200

    Added a counter component

--- app.js
+++ app.js
@@ -7,4 +7,29 @@ function Header() {
     );
 }
 
-ReactDOM.render(<Header />, document.getElementById('root'));
+const Counter = () => {
+    return (
+        <div className='counter'>
+            <button className='counter-action decreament'> - </button>
+            <span className='counter-score'> 40 </span>
+            <button className='counter-action increament'> + </button>
+        </div>
+    );
+};
+
+const Player = () => {
+    return (
+        <div className='player'>
+            <span className='player-name'>Moni</span>
+
+            {/* <div className='counter'>
+                <button className='counter-action decreament'> - </button>
+                <span className='counter-score'> 40 </span>
+                <button className='counter-action increament'> + </button>
+            </div> */}
+            <Counter />
+        </div>
+    );
+};
+
+ReactDOM.render(<Player />, document.getElementById('root'));
