6b5bc06e9463533d084b25f0328d8c9fec31412f
Moni <moniruzzaman.eng1995@gmail.com>
2022-03-07 10:37:13 +0200

    Added a Header component

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-const desc = 'I just learned how to create a React node and render it into the DOM';
-const myTitleID = 'main-title';
-const firstName = 'Moni';
+function Header() {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className='stats'>Players: 1</span>
+        </header>
+    );
+}
 
-const header = (
-    <header>
-        {/* This is comment */}
-        <h1>{firstName}'s first React Element</h1>
-        <p>{desc}</p>
-    </header>
-);
-
-ReactDOM.render(header, document.getElementById('root'));
+ReactDOM.render(<Header />, document.getElementById('root'));
