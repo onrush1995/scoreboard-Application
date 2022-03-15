40a7307ba1ed492f471656d191e8b4152f14a803
Moni <moniruzzaman.eng1995@gmail.com>
2022-03-15 14:25:29 +0200

    Added onClick event in the button handeler

--- app.js
+++ app.js
@@ -35,12 +35,18 @@ class Counter extends React.Component {
         score: 0
     };
 
+    incrementScore() {
+        console.log('Hi from increment score');
+    }
     render() {
         return (
             <div className='counter'>
                 <button className='counter-action decrement'> - </button>
                 <span className='counter-score'> {this.state.score} </span>
-                <button className='counter-action increment'> + </button>
+                <button className='counter-action increment' onClick={this.incrementScore}>
+                    {' '}
+                    +{' '}
+                </button>
             </div>
         );
     }
