39e955f71c7c893df475ae818e716ef123fde4d7
Moni <moniruzzaman.eng1995@gmail.com>
2022-03-15 14:43:01 +0200

    Modify increment/decrement method with arrow function

--- app.js
+++ app.js
@@ -35,27 +35,27 @@ class Counter extends React.Component {
         score: 0
     };
 
-    incrementScore() {
+    incrementScore = () => {
         this.setState({
             score: this.state.score + 1
         });
-    }
+    };
 
-    decrementScore() {
+    decrementScore = () => {
         this.setState({
             score: this.state.score - 1
         });
-    }
+    };
 
     render() {
         return (
             <div className='counter'>
-                <button className='counter-action decrement' onClick={() => this.decrementScore()}>
+                <button className='counter-action decrement' onClick={this.decrementScore}>
                     {' '}
                     -{' '}
                 </button>
                 <span className='counter-score'> {this.state.score} </span>
-                <button className='counter-action increment' onClick={() => this.incrementScore()}>
+                <button className='counter-action increment' onClick={this.incrementScore}>
                     +
                 </button>
             </div>
