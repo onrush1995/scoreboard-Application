ca979f2f5a9f214165077bcebd07fe83e5ad02a7
Moni <moniruzzaman.eng1995@gmail.com>
2022-03-17 11:38:54 +0200

    creating Application state

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-const players = [
-    {
-        name: 'Guil',
-        score: 50,
-        id: 1
-    },
-    {
-        name: 'Treasure',
-        score: 85,
-        id: 2
-    },
-    {
-        name: 'Ashley',
-        score: 95,
-        id: 3
-    },
-    {
-        name: 'James',
-        score: 80,
-        id: 4
-    }
-];
-
 const Header = (props) => {
     return (
         <header>
@@ -72,15 +49,37 @@ const Player = (props) => {
     );
 };
 
-const App = (props) => {
-    return (
-        <div className='scoreboard'>
-            <Header title='Scoreboard' totalPlayers={props.initialPlayers.length} />
-            {props.initialPlayers.map((player) => (
-                <Player name={player.name} score={player.score} key={player.id.toString()} />
-            ))}
-        </div>
-    );
-};
+class App extends React.Component {
+    state = {
+        players: [
+            {
+                name: 'Guil',
+                id: 1
+            },
+            {
+                name: 'Treasure',
+                id: 2
+            },
+            {
+                name: 'Ashley',
+                id: 3
+            },
+            {
+                name: 'James',
+                id: 4
+            }
+        ]
+    };
+    render() {
+        return (
+            <div className='scoreboard'>
+                <Header title='Scoreboard' totalPlayers={this.state.players.length} />
+                {this.state.players.map((player) => (
+                    <Player name={player.name} score={player.score} key={player.id.toString()} />
+                ))}
+            </div>
+        );
+    }
+}
 
-ReactDOM.render(<App initialPlayers={players} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
